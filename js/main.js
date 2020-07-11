76f56c52b5d55cf97f2bc8af6c15397c7b1e4d6a
artemiy228 <34098193+artemiy228@users.noreply.github.com>
2020-07-11 22:44:39 +0300

    update three section error

--- js/main.js
+++ js/main.js
@@ -15,10 +15,22 @@ $(document).ready(function () {
 		autoplay: false,
 		dots: false,
 	});
-	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
 		$(this)
 			.addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
 			.closest('div.container').find('div.catalog__content').removeClass('catalog__content-active').eq($(this).index()).addClass('catalog__content-active');
+	});
+
+	$('.before__slider').slick({
+		prevArrow: '<button class="slider-btn slider-btn__left"><img src=""></button>',
+		nextArrow: '<button class="slider-btn slider-btn__right"><img src=""></button>',
+
+	});
+
+	$('ul.before__tabs').on('click', 'li:not(.before__tab-active)', function () {
+		$(this)
+			.addClass('before__tab-active').siblings().removeClass('before__tab-active')
+			.closest('div.container').find('div.before__slider-section').removeClass('before__slider-section--active').eq($(this).index()).addClass('before__slider-section--active');
 	});
 
 });
